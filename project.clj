c0f9669a834c025e97b3dbe927668bea8ecbbf62
kzkaed <kzkaeding@gmail.com>
2015-04-19 16:34:01 -0500

    compojure for routes

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [ring "1.0.1"]
-                 [compojure "1.0.1"]
+                 [ring "1.3.2"]
+                 [compojure "1.3.3"]
                  [enlive "1.1.5"]])
